aeecf00774c6725e9e54082651786a513b1fad53
Tommy <Tommy@LAPTOP-V6VDID38>
2021-02-19 21:59:39 -0500

    committed to tomi and updated string

--- Arrays/src/working_with_arrays/Arrays_Practice.java
+++ Arrays/src/working_with_arrays/Arrays_Practice.java
@@ -5,23 +5,20 @@ public class Arrays_Practice {
 	public static void main(String[] args) {
 
 		// String [] names = {"Bob", "Josh", "Ivan"};
-		
 
 		char letters[] = new char[1];
 		double arry[] = new double[5];
 		long long_numbers[] = new long[8];
 		String bestOfThebest[] = new String[5];
-		
 
-		  
-		   
-		   String[] name = new String [3];
-			name [0] = "old one";
-			name [1] = " new one";
-	
-		   
-		   System.out.println(arry);
-           System.out.println(letters);
+		String tired = "Please take a break if you are tired";
+
+		String[] name = new String[3];
+		name[0] = "old one";
+		name[1] = " new one";
+
+		System.out.println(arry);
+		System.out.println(letters);
 	}
 
 }
