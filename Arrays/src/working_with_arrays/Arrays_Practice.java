e0aff5ea5e6155764a9c8c12625fd9fbd209943b
dmytroyurchyk <dimayrchuk@gmail.com>
2021-02-19 21:10:25 -0500

    changing long array

--- Arrays/src/working_with_arrays/Arrays_Practice.java
+++ Arrays/src/working_with_arrays/Arrays_Practice.java
@@ -8,7 +8,9 @@ public class Arrays_Practice {
 		// int even_numbers [] = { 8, 4, 6, 2 };
 		   char letters [] = new char [1];
 		   double arry [] = new double [5];
-		   long long_numbers [] = new long [8];
+		   long long_numbers [] = new long [8]
+		   
+		   
 
 	}
 
