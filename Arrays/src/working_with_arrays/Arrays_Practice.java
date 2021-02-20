25d9f3e3e25792e00cb315a024a483df50f296d8
Tommy <Tommy@LAPTOP-V6VDID38>
2021-02-19 21:09:28 -0500

    I made some changes to the cloned Array

--- Arrays/src/working_with_arrays/Arrays_Practice.java
+++ Arrays/src/working_with_arrays/Arrays_Practice.java
@@ -3,13 +3,14 @@ package working_with_arrays;
 public class Arrays_Practice {
 
 	public static void main(String[] args) {
-		
+
 		// String [] names = {"Bob", "Josh", "Ivan"};
 		// int even_numbers [] = { 8, 4, 6, 2 };
-		   char letters [] = new char [1];
-		   double arry [] = new double [5];
-		   long long_numbers [] = new long [8];
-
+		char letters[] = new char[1];
+		double arry[] = new double[5];
+		long long_numbers[] = new long[8];
+		String best[] = new String[5];
+		
 	}
 
 }
