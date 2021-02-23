85d211b183e47316b806850b90d86d0518f45b91
Madina <Madina@10.0.0.80>
2021-02-22 19:32:08 -0500

    i changed Strig name

--- Arrays/src/working_with_arrays/Arrays_Practice.java
+++ Arrays/src/working_with_arrays/Arrays_Practice.java
@@ -4,7 +4,7 @@ public class Arrays_Practice {
 
 	public static void main(String[] args) {
 
-		// String [] names = {"Bob", "Josh", "Ivan"};
+		// String [] names = {"Tob", "Lob", "Rob"};
 		
 
 		char letters[] = new char[1];
